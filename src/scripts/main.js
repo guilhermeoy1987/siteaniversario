4e4109ae27e24369144d3d187a4214cc411b693b
sakio87 <guilherme-oyama87@hotmail.com>
2026-01-31 15:58:15 -0300

    first commit

---
+++ src/scripts/main.js
@@ -0,0 +1,29 @@
+AOS.init({
+    duration: 1000, // 1000 milissegundos = 1 segundo de animação
+    once: true,     // Faz a animação acontecer apenas uma vez ao descer a página
+});
+const dataDoEvento = new Date("Sep 05, 2026 17:00:00");
+const timestampDoEvento = dataDoEvento.getTime();
+
+const contaAsHoras = setInterval(function() {
+    const agora = new Date();
+    const timestampAtual = agora.getTime();
+
+    const distanciaAteOEvento = timestampDoEvento - timestampAtual;
+
+    const diaEmMs = 1000 * 60 * 60 * 24;
+    const horaEmMs = 1000 * 60 * 60;
+    const minutoEmMs = 1000 * 60;
+
+    const diasAteOEvento = Math.floor(distanciaAteOEvento / diaEmMs);
+    const horasAteOEvento = Math.floor((distanciaAteOEvento % diaEmMs) / horaEmMs);
+    const minutosAteOEvento = Math.floor((distanciaAteOEvento % horaEmMs) / minutoEmMs);
+    const segundosAteOEvento = Math.floor((distanciaAteOEvento % minutoEmMs) / 1000);
+
+    document.getElementById('contador').innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`;
+
+    if (distanciaAteOEvento < 0) {
+        clearInterval(contaAsHoras);
+        document.getElementById('contador').innerHTML = 'Evento expirado';
+    }
+}, 1000);
